ab711a1fe1a5929b8a97336af8b3493a11c2851f
maxrusakovich <maxrusakovich@gmail.com>
2019-08-14 18:43:39 +0300

    React router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Route, Switch} from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
@@ -6,9 +7,17 @@ import { generatePalette } from './colorHelpers';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Palette palette={generatePalette(seedColors[1])}/>
-      </div>
+      <Switch>
+        <Route exact path='/' render={() => <h1>palette lsit</h1>}/>
+        <Route 
+          exact 
+          path='/palette/:id' 
+          render={() => <h1>individual palette</h1>} 
+        />
+      </Switch>
+      // <div>
+       // <Palette palette={generatePalette(seedColors[1])}/>
+      // </div>
     );
   }
 }
